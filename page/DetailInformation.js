8f39822b109df4434cf475843ef6d8de5bce1bc9
mangjapra <japraivo@gmail.com>
2024-03-07 01:21:23 +0700

    Update

--- page/DetailInformation.js
+++ page/DetailInformation.js
@@ -1,13 +1,5 @@
 import React, { useEffect } from "react";
-import {
-  Button,
-  Pressable,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { getPokemonById } from "../redux/global/action";
 
@@ -22,46 +14,47 @@ function DetailInformation({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      
       <View style={styles.headerContainer}>
         <Pressable
           onPress={() => navigation.goBack()}
           style={styles.backButton}
         >
           <Text style={styles.textButtonStyle}>
-            Name of Pokemon: {pokemonbyid.name}
+            Name of Pokemon: {pokemonbyid?.name}
           </Text>
           <Text style={styles.textButtonStyle}>Id of Pokemon: {pokemonId}</Text>
         </Pressable>
       </View>
 
       <ScrollView>
-        <View style={{ margin: 10 }}>
-          <Text>Abilities: </Text>
-          {pokemonbyid.abilities.map((res, index) => {
-            return <Text key={index}>{res.ability.name}</Text>;
-          })}
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <View style={{ margin: 10 }}>
+            <Text>Abilities: </Text>
+            {pokemonbyid?.abilities.map((res, index) => {
+              return <Text key={index}>{res?.ability?.name}</Text>;
+            })}
+          </View>
+          <View style={{ margin: 10 }}>
+            <Text>Types: </Text>
+            {pokemonbyid?.types.map((res, index) => {
+              return <Text key={index}>{res?.type?.name}</Text>;
+            })}
+          </View>
         </View>
-        <View style={{ margin: 10 }}>
-          <Text>Types: </Text>
-          {pokemonbyid.types.map((res, index) => {
-            return <Text key={index}>{res.type.name}</Text>;
-          })}
-        </View>
+        
         <View style={{ margin: 10 }}>
           <Text>Stats: </Text>
-          {pokemonbyid.stats.map((res, index) => {
+          {pokemonbyid?.stats.map((res, index) => {
             return (
-              <>
-                <Text>{res.stat.name}</Text>
-                <Text>{res.base_stat}</Text>
-                <Text>{res.effort}</Text>
-              </>
+              <View key={index}>
+                <Text>{res?.stat?.name}</Text>
+                <Text>{res?.base_stat}</Text>
+                <Text>{res?.effort}</Text>
+              </View>
             );
           })}
         </View>
       </ScrollView>
-
     </View>
   );
 }
